2c382e93f402eea3a03240d82c0c4e79d7ea2100
chong050 <chong050@morris.umn.edu>
2018-09-21 18:49:36 -0500

    Started dismvowel.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  char *result;
+  int size;
+  int len, i;
+
+  size = 100;
+  result = (char*) calloc (size + 1, sizeof(char));
+  len = strlen(str);
+ 
+  for(i=0; i < len; i++){
+	if(str[i] != 'a'){
+	result[i] = str[i];
+	}
+  }
+  result[len] = '\0';
+  return (char*) result;
 }
