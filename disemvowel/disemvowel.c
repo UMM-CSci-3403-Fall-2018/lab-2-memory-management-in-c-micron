dbda5f42071057dba414c3fb9bcd55ceb46328a3
chong050 <chong050@morris.umn.edu>
2018-09-25 10:30:32 -0500

    Created functional method for disemvowel. Works on test strings.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -5,15 +5,22 @@
 char *disemvowel(char *str) {
   char *result;
   int size;
-  int len, i;
+  int len, i, j;
 
   size = 100;
   result = (char*) calloc (size + 1, sizeof(char));
   len = strlen(str);
- 
-  for(i=0; i < len; i++){
-	if(str[i] != 'a'){
-	result[i] = str[i];
+  i = 0;
+  j = 0;
+  while(i < len){
+	if(str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u'||
+	   str[i] == 'A'||str[i] == 'E'||str[i] == 'I'||str[i] == 'O'||str[i] == 'U'
+	   ){
+	i++;
+	}else{
+	result[j] = str[i];
+	i++;
+	j++;
 	}
   }
   result[len] = '\0';
